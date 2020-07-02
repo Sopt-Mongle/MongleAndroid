b3049aa26d6d50732c2f435c62214fbf9873afc6
chloe73 <esom735@gmail.com>
2020-07-02 21:38:07 +0900

    somin branch commit

---
+++ app/src/main/java/com/example/mongleandroid/PostThemeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mongleandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PostThemeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post_theme)
+    }
+}
