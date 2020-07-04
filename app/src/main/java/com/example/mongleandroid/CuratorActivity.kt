c77badfca7ef04835639a90fab0f12b67be268ff
devhapeng <hayoung8602@gmail.com>
2020-07-05 00:41:20 +0900

    hayoung commit

--- app/src/main/java/com/example/mongleandroid/CuratorActivity.kt
+++ app/src/main/java/com/example/mongleandroid/CuratorActivity.kt
@@ -2,9 +2,83 @@ package com.example.mongleandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class CuratorActivity : AppCompatActivity() {
+
+    lateinit var rvCuratorClass: RecyclerView
+    lateinit var rvCuratorAdapter: CuratorAdapter
+    val datas = mutableListOf<CuratorData>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.curator_activity)
+
+        initCuratorRecyclerView()
+    }
+
+
+    fun initCuratorRecyclerView() {
+
+        rvCuratorClass = findViewById(R.id.rv_curator)
+        rvCuratorAdapter = CuratorAdapter(this)
+        rvCuratorClass.adapter = rvCuratorAdapter
+        rvCuratorClass.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+        rvCuratorAdapter.datas = listOf(
+            CuratorData(
+                img_profile_item = R.drawable.ic_launcher_foreground,
+                item_username = "소민이",
+                item_contents = "안녕 나는 뿌까라고 해 만나서 반가워 나는 웃는 게 예쁜 사람이야",
+                item_img_online = R.drawable.ic_launcher_foreground,
+                btn_subscribe_item = R.drawable.ic_launcher_foreground,
+                hashtag_curator = R.drawable.ic_launcher_foreground,
+                hashtag2_curator = R.drawable.ic_launcher_foreground
+
+            ),
+            CuratorData(
+                img_profile_item = R.drawable.ic_launcher_foreground,
+                item_username = "소민이",
+                item_contents = "안녕 나는 뿌까라고 해 만나서 반가워 나는 웃는 게 예쁜 사람이야",
+                item_img_online = R.drawable.ic_launcher_foreground,
+                btn_subscribe_item = R.drawable.ic_launcher_foreground,
+                hashtag_curator = R.drawable.ic_launcher_foreground,
+                hashtag2_curator = R.drawable.ic_launcher_foreground
+
+            ),
+            CuratorData(
+                img_profile_item = R.drawable.ic_launcher_foreground,
+                item_username = "소민이",
+                item_contents = "안녕 나는 뿌까라고 해 만나서 반가워 나는 웃는 게 예쁜 사람이야",
+                item_img_online = R.drawable.ic_launcher_foreground,
+                btn_subscribe_item = R.drawable.ic_launcher_foreground,
+                hashtag_curator = R.drawable.ic_launcher_foreground,
+                hashtag2_curator = R.drawable.ic_launcher_foreground
+
+            ),
+            CuratorData(
+                img_profile_item = R.drawable.ic_launcher_foreground,
+                item_username = "소민이",
+                item_contents = "안녕 나는 뿌까라고 해 만나서 반가워 나는 웃는 게 예쁜 사람이야",
+                item_img_online = R.drawable.ic_launcher_foreground,
+                btn_subscribe_item = R.drawable.ic_launcher_foreground,
+                hashtag_curator = R.drawable.ic_launcher_foreground,
+                hashtag2_curator = R.drawable.ic_launcher_foreground
+
+            ),
+            CuratorData(
+                img_profile_item = R.drawable.ic_launcher_foreground,
+                item_username = "소민이",
+                item_contents = "안녕 나는 뿌까라고 해 만나서 반가워 나는 웃는 게 예쁜 사람이야",
+                item_img_online = R.drawable.ic_launcher_foreground,
+                btn_subscribe_item = R.drawable.ic_launcher_foreground,
+                hashtag_curator = R.drawable.ic_launcher_foreground,
+                hashtag2_curator = R.drawable.ic_launcher_foreground
+
+            )
+        ).toMutableList()
+        rvCuratorAdapter.notifyDataSetChanged()
     }
 }
+
