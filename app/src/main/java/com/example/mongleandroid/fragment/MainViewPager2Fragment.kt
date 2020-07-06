72f15a1b052401f098b527a760c425bc9b871dd1
chloe73 <esom735@gmail.com>
2020-07-06 22:54:34 +0900

    [소민]메인 프래그먼트뷰 xml 작업

--- app/src/main/java/com/example/mongleandroid/fragment/MainViewPager2Fragment.kt
+++ app/src/main/java/com/example/mongleandroid/fragment/MainViewPager2Fragment.kt
@@ -5,20 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.mongleandroid.R
-import com.example.mongleandroid.adapter.MainPagerAdapter
-import kotlinx.android.synthetic.main.fragment_main.*
 
-class MainFragment : Fragment() {
+import com.example.mongleandroid.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class MainViewPager2Fragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        return inflater.inflate(R.layout.fragment_main_view_pager2, container, false)
     }
-
-
 
 }
