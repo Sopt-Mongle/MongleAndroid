5c6420d87f462b0effe75d5dbf80490b433dd3dd
sery270 <sery270@naver.com>
2020-07-10 14:36:37 +0900

    [view] activity_writing_sentence.xml - 문장 만들기 뷰 만들기

---
+++ app/src/main/java/com/example/mongleandroid/activity/WritingSentenceFinishActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mongleandroid.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mongleandroid.R
+
+class WritingSentenceFinishActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_writing_sentence_finish)
+    }
+}
