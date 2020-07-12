06e64bd1880f37d4c5e9f77d7f45f62afe9333a8
jinyand <jiiin97@naver.com>
2020-07-12 23:00:26 +0900

    [현진] 테마에서 문장쓰기 뷰 작업 완료

---
+++ app/src/main/java/com/example/mongleandroid/activity/ThemeWritingSentenceBookActivity.kt
@@ -0,0 +1,41 @@
+package com.example.mongleandroid.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.mongleandroid.R
+import kotlinx.android.synthetic.main.activity_theme_writing_sentence.*
+import kotlinx.android.synthetic.main.activity_theme_writing_sentence_book.*
+
+class ThemeWritingSentenceBookActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_theme_writing_sentence_book)
+
+        // 책 추가
+        activity_theme_writing_sentence_book_tv_title.setOnClickListener {
+            val intent = Intent(this, WritingSentenceBookSearchActivity::class.java)
+            startActivity(intent)
+            // 이거 아마 startActivityForResult로 바꿔야 함!!
+        }
+
+        // 책 제목 text가 없는 경우 경고창
+        activity_theme_writing_sentence_book_btn_upload.setOnClickListener {
+            if(activity_theme_writing_sentence_book_tv_title.text.toString().length <= 0) {
+                activity_theme_writing_sentence_book_tv_title.background = getResources().getDrawable(R.drawable.et_area_red)
+                activity_theme_writing_sentence_book_img_warning.visibility = View.VISIBLE
+                activity_theme_writing_sentence_book_tv_warning.visibility = View.VISIBLE
+            } else {
+                // 있는 경우
+                // 다음 액티비티로 이동 (문장 등록 끝)
+                activity_theme_writing_sentence_book_tv_title.background = getResources().getDrawable(R.drawable.et_area)
+                activity_theme_writing_sentence_book_img_warning.visibility = View.GONE
+                activity_theme_writing_sentence_book_tv_warning.visibility = View.GONE
+            }
+        }
+
+
+
+    }
+}
--- app/src/main/java/com/example/mongleandroid/adapter/viewholder/ResultThemeViewHolder.kt
+++ app/src/main/java/com/example/mongleandroid/adapter/viewholder/ResultThemeViewHolder.kt
@@ -1,11 +1,18 @@
 package com.example.mongleandroid.adapter.viewholder
 
+import android.graphics.Color
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
+import android.util.Log
 import android.view.View
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mongleandroid.R
+import com.example.mongleandroid.activity.MainActivity
 import com.example.mongleandroid.network.data.ResultThemeData
 import kotlinx.android.synthetic.main.item_result_theme.*
+import java.lang.StringBuilder
 
 class ResultThemeViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
 
@@ -14,9 +21,30 @@ class ResultThemeViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView)
     val result_theme_tv_sentence_count = itemView.findViewById<TextView>(R.id.result_theme_tv_sentence_count)
 
     fun bind(resultThemeData: ResultThemeData){
-        result_theme_tv_theme.text = resultThemeData.result_theme_tv_theme
         result_theme_tv_bookmark.text = resultThemeData.result_theme_tv_bookmark
         result_theme_tv_sentence_count.text = resultThemeData.result_theme_tv_sentence_count
+
+        val inputString = MainActivity.search_result
+        val sb = StringBuilder(inputString)
+        val startWord = sb.get(0)
+        val endWord = sb.get(sb.lastIndex)
+        Log.d("index","startWord : ${startWord}, endWord ${endWord}")
+
+
+        val startIndex = resultThemeData.result_theme_tv_theme.indexOf(startWord)
+        val lastIndex = resultThemeData.result_theme_tv_theme.lastIndexOf(endWord)
+        Log.d("index","startIndex : ${startIndex}, lastIndex ${lastIndex}")
+        val spannable = SpannableStringBuilder(resultThemeData.result_theme_tv_theme)
+        spannable.setSpan(
+            ForegroundColorSpan(Color.RED),
+            startIndex,
+            lastIndex +1, // end
+            Spannable.SPAN_EXCLUSIVE_INCLUSIVE
+        )
+        result_theme_tv_theme.text = spannable
+
+//        if(inputString.length != lastIndex - startIndex)
+
     }
 }
 
