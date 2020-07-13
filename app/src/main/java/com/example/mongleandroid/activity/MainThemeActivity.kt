8bbaeee1fc41bada2841f78a5613e9b53bb0af06
chloe73 <esom735@gmail.com>
2020-07-14 04:50:45 +0900

    [소민] #48 : MainTheme Activity - 리사이클러뷰 구현 완료

--- app/src/main/java/com/example/mongleandroid/activity/MainThemeActivity.kt
+++ app/src/main/java/com/example/mongleandroid/activity/MainThemeActivity.kt
@@ -3,10 +3,73 @@ package com.example.mongleandroid.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mongleandroid.R
+import com.example.mongleandroid.adapter.MainThemeAdapter
+import com.example.mongleandroid.network.data.MainThemeData
+import kotlinx.android.synthetic.main.activity_main_theme.*
+import kotlinx.android.synthetic.main.item_activity_theme.*
 
 class MainThemeActivity : AppCompatActivity() {
+
+    private var data = mutableListOf<MainThemeData>()
+
+    private lateinit var mainThemeAdapter : MainThemeAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_theme)
+        setMainThemeAdapter(data)
+    }
+    private fun setMainThemeAdapter(mainThemeItem: MutableList<MainThemeData>) {
+        mainThemeAdapter =
+            MainThemeAdapter(
+                mainThemeItem,
+                this
+            )
+        loadDatas()
+        rv_activity_theme.adapter = mainThemeAdapter
+    }
+    private fun loadDatas() {
+        data.apply {
+            add(
+                MainThemeData(
+                    tv_theme_sentence = "영 영앤 리치 남자를 만나자 감 감사하자",
+                    tv_theme_curator = "몽글이 몽글몽글",
+                    tv_like_num = "100",
+                    tv_save_num = "30"
+                )
+            )
+            add(
+                MainThemeData(
+                    tv_theme_sentence = "영 영앤 리치 남자를 만나자 감 감사하자",
+                    tv_theme_curator = "몽글이 몽글몽글",
+                    tv_like_num = "10",
+                    tv_save_num = "300"
+                )
+            )
+            add(
+                MainThemeData(
+                    tv_theme_sentence = "영 영앤 리치 남자를 만나자",
+                    tv_theme_curator = "몽글이 몽글몽글",
+                    tv_like_num = "109",
+                    tv_save_num = "305"
+                )
+            )
+            add(
+                MainThemeData(
+                    tv_theme_sentence = "영 영앤 리치 남자를 만나자",
+                    tv_theme_curator = "몽글이 몽글몽글",
+                    tv_like_num = "1030",
+                    tv_save_num = "30"
+                )
+            )
+            add(
+                MainThemeData(
+                    tv_theme_sentence = "영 영앤 리치 남자를 만나자",
+                    tv_theme_curator = "몽글이 몽글몽글",
+                    tv_like_num = "100",
+                    tv_save_num = "340"
+                )
+            )
+        }
     }
 }
