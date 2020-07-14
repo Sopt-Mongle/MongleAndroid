2452559abc6371bc9c371256e587cb85bcfa8ae8
devhapeng <hayoung8602@gmail.com>
2020-07-14 10:35:20 +0900

    [hayoung] 내 서재 뷰 : 스크롤뷰와 뷰페이저 간의 전쟁 중

--- app/src/main/java/com/example/mongleandroid/adapter/viewholder/LibraryCuratorViewHolder.kt
+++ app/src/main/java/com/example/mongleandroid/adapter/viewholder/LibraryCuratorViewHolder.kt
@@ -1,6 +1,7 @@
 package com.example.mongleandroid.adapter.viewholder
 
 import android.view.View
+import android.widget.CheckBox
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -16,6 +17,8 @@ class LibraryCuratorViewHolder(itemView: View) : RecyclerView.ViewHolder(itemVie
         itemView.findViewById<TextView>(R.id.tv_library_curator_subcount)
     val tv_library_curator_keyword =
         itemView.findViewById<TextView>(R.id.tv_library_curator_keyword)
+    val library_curator_btn_subscribe_item =
+        itemView.findViewById<CheckBox>(R.id.library_curator_btn_subscribe_item)
 
     fun bind(libraryCuratorData: LibraryCuratorData) {
         Glide.with(itemView).load(libraryCuratorData.library_curator_img_profile)
@@ -23,5 +26,11 @@ class LibraryCuratorViewHolder(itemView: View) : RecyclerView.ViewHolder(itemVie
         tv_library_curator_name.text = libraryCuratorData.tv_library_curator_name
         tv_library_curator_subcount.text = libraryCuratorData.tv_library_curator_subcount
         tv_library_curator_keyword.text = libraryCuratorData.tv_library_curator_keyword
+        library_curator_btn_subscribe_item.setOnClickListener {
+            if (library_curator_btn_subscribe_item.isChecked) {
+                library_curator_btn_subscribe_item.setText("구독중")
+            } else library_curator_btn_subscribe_item.setText("구독")
+
+        }
     }
 }
