36e997b374125166e00fff398ff3f226802a52e6
devhapeng <hayoung8602@gmail.com>
2020-07-14 14:25:44 +0900

    [hayoung] 내 서재 뷰 : 스크롤뷰와 뷰페이저 간의 전쟁 중_이거 말고 호준님한테 보낸거가 스크롤이 됨.

--- app/src/main/java/com/example/mongleandroid/adapter/viewholder/LibraryThemaViewHolder.kt
+++ app/src/main/java/com/example/mongleandroid/adapter/viewholder/LibraryThemaViewHolder.kt
@@ -11,10 +11,26 @@ class LibraryThemaViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
     val tv_thema_num_library = itemView.findViewById<TextView>(R.id.tv_thema_num_library)
     val tv_sentence_count_library_item =
         itemView.findViewById<TextView>(R.id.tv_sentence_count_library_item)
+//    val rdbtn_saved_thema = itemView.findViewById<RadioButton>(R.id.rdbtn_saved_thema)
+//    val rdbtn_making_thema = itemView.findViewById<RadioButton>(R.id.rdbtn_making_thema)
 
     fun bind(libraryThemaData: LibraryThemaData) {
         tv_library_thema.text = libraryThemaData.tv_library_thema
         tv_thema_num_library.text = libraryThemaData.tv_thema_num_library
         tv_sentence_count_library_item.text = libraryThemaData.tv_sentence_count_library_item
+
+//        rdbtn_saved_thema.setOnClickListener {
+//            if (rdbtn_saved_thema.isChecked) {
+//                rdbtn_saved_thema.setTextColor(Color.WHITE)
+//            } else rdbtn_saved_thema.setTextColor(Color.LTGRAY)
+//
+//        }
+//
+//        rdbtn_making_thema.setOnClickListener {
+//            if (rdbtn_making_thema.isChecked) {
+//                rdbtn_making_thema.setTextColor(Color.LTGRAY)
+//            } else rdbtn_making_thema.setTextColor(Color.WHITE)
+//
+//        }
     }
 }
