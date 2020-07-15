1193eb70807fd996ba2924b3ed8465f8b2bd62c6
jinyand <jiiin97@naver.com>
2020-07-15 14:12:43 +0900

    [현진] LoginActivity - Dialog 팝업창 완료

--- app/src/main/java/com/example/mongleandroid/activity/LoginActivity.kt
+++ app/src/main/java/com/example/mongleandroid/activity/LoginActivity.kt
@@ -1,14 +1,17 @@
 package com.example.mongleandroid.activity
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import com.example.mongleandroid.DialogLogin
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mongleandroid.util.DialogLogin
 import com.example.mongleandroid.R
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.dialog_login.*
 
 class LoginActivity : AppCompatActivity(), View.OnClickListener {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -17,17 +20,19 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
             val intent = Intent(this, JoinActivity::class.java)
             startActivity(intent)
         }
+
         activity_login_btn_login.setOnClickListener(this)
+
     }
 
     override fun onClick(v: View?) {
         when(v?.id) {
             activity_login_btn_login.id -> {
                 val dlg = DialogLogin(this)
-                dlg.setOnOKClickedListener{
+                dlg.setOnOKClickedListener{ content ->
 
                 }
-                dlg.start("메인?")
+                dlg.start("")
             }
         }
     }
