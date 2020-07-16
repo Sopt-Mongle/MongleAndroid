99930a71affad524ef91cf80f90fd91e8fdc2d65
sery270 <sery270@naver.com>
2020-07-16 14:37:50 +0900

    [view] WritingThemeActivity - 테마 만들기 뷰 만들기 #90 선택 이벤트 처리 완료

--- app/src/main/java/com/example/mongleandroid/adapter/WritingThemeAdapter.kt
+++ app/src/main/java/com/example/mongleandroid/adapter/WritingThemeAdapter.kt
@@ -2,16 +2,16 @@ package com.example.mongleandroid.adapter
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mongleandroid.R
 import com.example.mongleandroid.adapter.viewholder.WritingThemeViewHolder
 import com.example.mongleandroid.network.data.request.RequestWritingThemeData
-import com.example.mongleandroid.network.data.response.ResponseWritingThemeData
 
 class WritingThemeAdapter(private val context : Context) : RecyclerView.Adapter<WritingThemeViewHolder>()  {
 
-    var datas = mutableListOf<Int>()
+    var datas = mutableListOf<RequestWritingThemeData>()
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
@@ -27,7 +27,26 @@ class WritingThemeAdapter(private val context : Context) : RecyclerView.Adapter<
 
     override fun onBindViewHolder(holder: WritingThemeViewHolder, position: Int) {
         holder.bind(datas[position])
+
+        holder.itemView.setOnClickListener {
+            itemClickListener.onClick(it, position)
+
+        }
     }
 
+    //클릭 인터페이스 정의
+    interface ItemClickListener{
+        fun onClick(view: View, position: Int)
+    }
+
+    //클릭리스너 선언
+    private lateinit var itemClickListener: ItemClickListener
+
+    //클릭리스너 등록 메소드
+    fun setItemClickListener(itemClickListener: ItemClickListener){
+        this.itemClickListener = itemClickListener
+
+    }
 
 }
+
--- app/src/main/java/com/example/mongleandroid/adapter/viewholder/WritingThemeViewHolder.kt
+++ app/src/main/java/com/example/mongleandroid/adapter/viewholder/WritingThemeViewHolder.kt
@@ -1,27 +1,27 @@
 package com.example.mongleandroid.adapter.viewholder
 
-import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
-import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.mongleandroid.R
-import kotlin.math.log
+import com.example.mongleandroid.network.data.request.RequestWritingThemeData
 
 
 class WritingThemeViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView)  {
+
+    //서버에서 온 데이터를 처리하는 곳이 아님
+
 
     val activity_writing_theme_img_theme = itemView.findViewById<ImageView>(R.id.activity_writing_theme_img_theme)
     //val activity_writing_theme_et_theme_title = itemView.findViewById<EditText>(R.id.activity_writing_theme_et_theme_title)
 
 
-    fun bind(themeImgIdx: Int){
-        //val img : String = "drawable-xxxhdpi/maketheme_img_theme_" + themeImgIdx.toString() + ".png"
-        //var img : Int = R.drawable.maketheme_img_theme_1
-        //Log.d("themeImgIdx", themeImgIdx.toString())
-        Glide.with(itemView).load(themeImgIdx).into(activity_writing_theme_img_theme)
+    fun bind(requestWritingThemeData: RequestWritingThemeData){
+        when(requestWritingThemeData.themeImgIdx){
+            1-> Glide.with(itemView).load(requestWritingThemeData.themeImgIdx).into(activity_writing_theme_img_theme)
+
+        }
     }
 }
