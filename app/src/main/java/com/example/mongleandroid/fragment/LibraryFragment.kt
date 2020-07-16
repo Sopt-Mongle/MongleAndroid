8fed2cba8470ad5f89a118ed6bc4a0f1f988b599
devhapeng <hayoung8602@gmail.com>
2020-07-16 22:58:26 +0900

    [hayoung] LibraryFragment - customTabLayout 완료

--- app/src/main/java/com/example/mongleandroid/fragment/LibraryFragment.kt
+++ app/src/main/java/com/example/mongleandroid/fragment/LibraryFragment.kt
@@ -6,6 +6,8 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.viewpager.widget.ViewPager
 import com.example.mongleandroid.R
@@ -43,11 +45,37 @@ class LibraryFragment : Fragment() {
         val libraryTabLayout = view.findViewById(R.id.titleLayout) as TabLayout
         val libraryViewPager = view.findViewById(R.id.vp_library) as ViewPager
         val adapter = LibraryTabAdapter(childFragmentManager)
+
+        val viewFirst : View = getLayoutInflater().inflate(R.layout.custom_library_tab_basic, null)
+        val viewSecond : View = getLayoutInflater().inflate(R.layout.custom_library_tab_basic, null)
+        val viewThird : View = getLayoutInflater().inflate(R.layout.custom_library_tab_basic, null)
+        val txtUpper1 : TextView = viewFirst.findViewById(R.id.library_tab_num)
+        val txtDown1 : TextView = viewFirst.findViewById(R.id.library_tab)
+        val txtUpper2 : TextView = viewSecond.findViewById(R.id.library_tab_num)
+        val txtDown2 : TextView = viewSecond.findViewById(R.id.library_tab)
+        val txtUpper3 : TextView = viewThird.findViewById(R.id.library_tab_num)
+        val txtDown3 : TextView = viewThird.findViewById(R.id.library_tab)
+
+        txtUpper1.setText("47")
+        txtDown1.setText("테마")
+
+        txtUpper2.setText("36")
+        txtDown2.setText("문장")
+
+        txtUpper3.setText("27")
+        txtDown3.setText("큐레이터")
+
         libraryViewPager.setAdapter(adapter)
         libraryTabLayout.setupWithViewPager(libraryViewPager)
-        libraryTabLayout.getTabAt(0)!!.setText("테마")
+        /*libraryTabLayout.getTabAt(0)!!.setText("테마")
         libraryTabLayout.getTabAt(1)!!.setText("문장")
         libraryTabLayout.getTabAt(2)!!.setText("큐레이터")
+*/
+        libraryTabLayout.getTabAt(0)!!.customView = viewFirst
+        libraryTabLayout.getTabAt(1)!!.customView = viewSecond
+        libraryTabLayout.getTabAt(2)!!.customView = viewThird
+
+
 
 
     }
