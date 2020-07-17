ae014c87579593d25e65a421681d725e04f22d62
jinyand <jiiin97@naver.com>
2020-07-17 20:41:30 +0900

    [현진] 로그인 화면 수정

--- app/src/main/java/com/example/mongleandroid/network/data/response/ResponseJoinData.kt
+++ app/src/main/java/com/example/mongleandroid/network/data/response/ResponseJoinData.kt
@@ -3,5 +3,9 @@ package com.example.mongleandroid.network.data.response
 data class ResponseJoinData (
     val status : Int,
     val success : Boolean,
-    val message : String
+    val message : String,
+    val data : Join
+)
+data class Join(
+    val accessToken : String
 )
